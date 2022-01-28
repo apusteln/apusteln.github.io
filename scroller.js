76f9c9e35559df2795a336d4095ef9d32e485076
A A <a@a.com>
2022-01-29 00:08:18 +0100

    Adding assets, wonder if this'll work

--- scroller.js
+++ scroller.js
@@ -52,11 +52,9 @@ function create() {
 
 function preload ()
 {
-	this.load.setBaseURL('http://labs.phaser.io');
-
-	this.load.image('sky', 'assets/skies/space3.png');
-	this.load.image('logo', 'assets/sprites/phaser1.png');
-	this.load.image('red', 'assets/particles/red.png');
+	this.load.image('sky', 'assets/space3.png');
+	this.load.image('logo', 'assets/phaser1.png');
+	this.load.image('red', 'assets/red.png');
 }
 
 function create ()
